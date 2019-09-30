0c71fa26588be9b8d46e2f4d9fb2cdc96d89b08f
JiaChang.Li <1769264507@qq.com>
2019-09-30 15:09:24 +0800

    MOD: 优化布局

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,22 +1,17 @@
-import {PageHeader} from 'antd';
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-
-//import {connectAlita} from 'redux-alita'
 
 class VideoPlayer extends Component {
 
     static propTypes = {
         url: PropTypes.string.isRequired,
         title: PropTypes.string.isRequired,
-        time:PropTypes.string.isRequired,
-        videoView:PropTypes.string.isRequired,
+        time: PropTypes.string.isRequired,
+        videoView: PropTypes.string.isRequired,
     }
 
     render() {
         console.log('url:', this.props.url)
-        // const {video_src = {}} = this.props.alitaState
-        // console.log(video_src)
         return (
             <div>
                 <h1>{this.props.title}</h1>
@@ -24,7 +19,7 @@ class VideoPlayer extends Component {
                 <p>播放量：{this.props.videoView}</p>
                 <video width='90%' height='600' controls={"controls"}>
                     {this.props.url ?
-                        <source src={this.props.url}/>
+                        <source src={this.props.url} />
                         : <p> 无效资源 </p>
                     }
                 </video>
--- src/page/vodpage/VodContent.jsx
+++ src/page/vodpage/VodContent.jsx
@@ -3,42 +3,42 @@ import './style.less';
 import VideoPlayer from '../../components/VideoPlayer/VideoPlayer'
 import VideoComment from '../../components/VideoPlayer/VideoComment'
 import VideoList from '../../components/VideoPlayer/VideoList'
-import {VCloudAPI} from "../../axios/apis";
+import { VCloudAPI } from "../../axios/apis";
 import PropTypes from 'prop-types';
 import VideoIntro from '../../components/VideoPlayer/VideoIntro'
 class VodContent extends React.Component {
     state = {
-        resource:{}
+        resource: {}
     }
     static propTypes = {
-        rid:PropTypes.string.isRequired,
+        rid: PropTypes.string.isRequired,
     }
-    componentWillMount(){
-        VCloudAPI.get('/com/'+'qWIaiFZTqkwbww6B'+'/resourse?rid='+this.props.rid).then(res=>{
-            console.log('res:',res)
-            this.setState({resource:res.data.data.resourse})
-            // this.props.setAlitaState({
-            //     stateName:'vide_src',
-            //     data:res.data.data.resourse
-            // })
-            console.log('resource:',this.state.resource)
+    componentWillMount() {
+        VCloudAPI.get('/com/' + 'qWIaiFZTqkwbww6B' + '/resourse/?rid=' + this.props.rid).then(res => {
+            console.log('res:', res)
+            this.setState({ resource: res.data.data.resourse })
+            console.log('resource:', this.state.resource)
         })
     }
     render() {
         return (<div className="vod-content">
             <div className="left-content">
                 <div className="video-content">
-                    <VideoPlayer url={this.state.resource.res_url} title={this.state.resource.name} time={this.state.resource.time} videoView={this.state.resource.video_view}/>
+                    <VideoPlayer
+                        url={this.state.resource.res_url}
+                        title={this.state.resource.name}
+                        time={this.state.resource.time}
+                        videoView={this.state.resource.video_view} />
                 </div>
                 <div className="intro-content">
-                    <VideoIntro intro={this.state.resource.intro}/>
+                    <VideoIntro intro={this.state.resource.intro} />
                 </div>
                 <div className="comment-content">
-                    <VideoComment rid={this.props.rid}/>
+                    <VideoComment rid={this.props.rid} />
                 </div>
             </div>
             <div className="right-content">
-                <VideoList/>
+                <VideoList />
             </div>
 
         </div>)
